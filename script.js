d6bb896ac91004df773fa6ec74a8801a9a097b25
Christopher Bowman <chrisbowmanfl@gmail.com>
2023-09-25 22:43:53 +0900

    Add computerChoice variable

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function computerChoice() {
+function generateComputer() {
   const result = ["rock", "scissors", "paper"];
   return result[Math.floor(Math.random() * result.length)];
 }
 
 let playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+let computerChoice = generateComputer();
 
-function game(computerChoice(), playerChoice) {
+function game(computerChoice, playerChoice) {
   if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
     return "Invalid";
   }
@@ -15,7 +16,4 @@ function game(computerChoice(), playerChoice) {
 }
 
 
-
-console.log(playerChoice);
-console.log(computerChoice());
 console.log(game(computerChoice, playerChoice));
